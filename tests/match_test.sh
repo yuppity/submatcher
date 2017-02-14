142afd15017e79e5ac6c799f82753b674a73f1dc
yuppity <yuppity@wubbalubba.com>
2017-02-14 14:22:07 +0200

    Start work on new test

---
+++ tests/match_test.sh
@@ -0,0 +1,43 @@
+#!/usr/bin/env bash
+
+declare -a matches
+matches+=(
+  "Community - 02x19 - Critical Film Studies.720p.WEB-DL.HoodBag.English.C.orig.Addic7ed.com.srt"
+  "Community S03E02 1080p WEB-DL AAC2.0 AVC-TrollHD.mp4"
+  "02" "19" ""
+)
+
+echo "match0: ${matches[0]}"
+echo "match1: ${matches[1]}"
+echo "match2: ${matches[2]}"
+echo "match3: ${matches[3]}"
+echo "match4: ${matches[4]}"
+
+#Community - 03x01 - Biology 101.720p.WEB-DL.HoodBag.English.C.orig.Addic7ed.com.srt
+#Community S03E01 1080p WEB-DL AAC2.0 AVC-TrollHD.mp4
+#Community S03E02 1080p WEB-DL AAC2.0 AVC-TrollHD.mp4
+#Community S03E03 1080p WEB-DL AAC2.0 AVC-TrollHD.mp4
+#Community S03E04 1080p WEB-DL AAC2.0 AVC-TrollHD.mp4
+#Community S03E05 1080p WEB-DL AAC2.0 AVC-TrollHD.mp4
+#Community S03E06 1080p WEB-DL AAC2.0 AVC-TrollHD.mp4
+#Community S03E07 1080p WEB-DL AAC2.0 AVC-TrollHD.mp4
+#Community S03E08 1080p WEB-DL AAC2.0 AVC-TrollHD.mp4
+#Community S03E09 1080p WEB-DL AAC2.0 AVC-TrollHD.mp4
+#Community S03E10 1080p WEB-DL AAC2.0 AVC-TrollHD.mp4
+#Community S03E11 1080p WEB-DL AAC2.0 AVC-TrollHD.mp4
+#Community S03E12 1080p WEB-DL AAC2.0 AVC-TrollHD.mp4
+#Community S03E13 1080p WEB-DL AAC2.0 AVC-TrollHD.mp4
+#Community S03E14 1080p WEB-DL AAC2.0 AVC-TrollHD.mp4
+#Community S03E15 1080p WEB-DL AAC2.0 AVC-TrollHD.mp4
+#Community S03E16 1080p WEB-DL AAC2.0 AVC-TrollHD.mp4
+#Community S03E17 1080p WEB-DL AAC2.0 AVC-TrollHD.mp4
+#Community S03E18 1080p WEB-DL AAC2.0 AVC-TrollHD.mp4
+#Community S03E19 1080p WEB-DL AAC2.0 AVC-TrollHD.mp4
+#Community S03E20 1080p WEB-DL AAC2.0 AVC-TrollHD.mp4
+#Community S03E21 1080p WEB-DL AAC2.0 AVC-TrollHD.mp4
+#Community S03E22 1080p WEB-DL AAC2.0 AVC-TrollHD.mp4
+
+#Community - 03x01 - Biology 101.720p.WEB-DL.HoodBag.English.C.orig.Addic7ed.com.srt
+#Community S03E01 1080p WEB-DL AAC2.0 AVC-TrollHD.mp4
+#Community S03E02 1080p WEB-DL AAC2.0 AVC-TrollHD.mp4
+##perl -pe 's/.*?([0-9]{2}).?([0-9]{2}).*/\1\2/p;'
